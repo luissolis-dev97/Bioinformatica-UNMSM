9811e014952e851d2213f8513d04d425078eb409
luissolis-dev97 <luis.solis.myp@gmail.com>
2026-05-24 18:26:33 -0500

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
-# Actualizar e instalar herramientas
+# 1. Actualizar repositorios
 sudo apt-get update
-sudo apt-get install -y sra-toolkit fastqc multiqc samtools bcftools bwa
 
-# Configurar SRA Toolkit para que no pregunte nada y guarde en el repositorio
+# 2. Instalar herramientas base
+sudo apt-get install -y fastqc multiqc samtools bcftools bwa curl
+
+# 3. Descargar e instalar SRA Toolkit manualmente (más seguro)
+mkdir -p /tmp/sratoolkit
+cd /tmp/sratoolkit
+curl -O https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/current/sratoolkit.current-ubuntu64.tar.gz
+tar -vxzf sratoolkit.current-ubuntu64.tar.gz
+# Mover binarios a una carpeta en el PATH
+sudo cp /tmp/sratoolkit/sratoolkit.*-ubuntu64/bin/* /usr/local/bin/
+
+# 4. Configuración rápida
 mkdir -p $HOME/.ncbi
 echo '/repository/user/main/public/root = "./data"' > $HOME/.ncbi/user-settings.mkfg
+
+echo "¡Instalación completada!"
